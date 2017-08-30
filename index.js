305146cb282aaa5b078d04f4ad83fc31b86c220c
ericpcho <ericpcho@gmail.com>
2017-08-30 14:30:41 -0700

    working light box with view channel page

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 'use strict';
 const GITHUB_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const api_key = 'AIzaSyC9NYtV2Dup8S78zqCQ3x1dPjazLRcVIAo'
+const STORE = []
 
 function getDataFromApi(searchTerm, callback) {
   const query = {
     part: 'snippet',
-    key: 'AIzaSyC9NYtV2Dup8S78zqCQ3x1dPjazLRcVIAo',
+    key: api_key,
     q: `${searchTerm}`
-    //per_page: 5
   }
   $.getJSON(GITHUB_SEARCH_URL, query, callback);
 }
 
-function renderResult(result) {
+function renderResult(item) {
   return `
-    <div>
-      <h2>
-      <img src=${result.snippet.thumbnails.medium.url}>
-    </div>
+      <a data-videoid="${item.id.videoId}" class="playvideo" href="#"><img src=${item.snippet.thumbnails.medium.url}></a>
+      <a class="viewchannel" href="https://www.youtube.com/channel/${item.snippet.channelId}">View Channel</a>
   `;
 }
 
+// {item.id.videoId}
+
 function displayYoutubeSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
+  data.items.map((item, index) => STORE.push(item));
+  const results = STORE.map(item => renderResult(item));
   $('.js-search-results').html(results);
 }
+
+
 
 function watchSubmit() {
   $('.js-search-form').submit(event => {
@@ -33,7 +37,22 @@ function watchSubmit() {
     // clear out the input
     queryTarget.val("");
     getDataFromApi(query, displayYoutubeSearchData);
+    $('.js-search-results').removeClass("hidden");
   });
 }
 
-$(watchSubmit);
+function clickThumbnail(){
+  $('.js-search-results').on("click", ".playvideo", event => {
+    const videoId = $(event.currentTarget).data('videoid')
+    $(".js-lightbox-display").html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`)
+  })
+}
+
+function handleEvents() {
+  watchSubmit();
+  clickThumbnail();
+}
+
+$(handleEvents);
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/YQHsXMglC9A" frameborder="0" allowfullscreen></iframe> */}
